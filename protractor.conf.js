97965791d41eb6910efcd599217829e6d4949f19
Abid Ali <abid.ali@Imzee.local>
2018-11-12 22:04:49 +0500

    only macOS, safari!

--- protractor.conf.js
+++ protractor.conf.js
@@ -90,13 +90,13 @@ const platformConfigurations = [
   //   version: "latest",
   //   seleniumVersion: '3.12.0'
   // },
-  {
+  /*{
     browserName: "chrome",
     platform: "Windows 10",
     version: "latest",
     seleniumVersion: '3.12.0',
     'tunnel-identifier': `${process.env.TRAVIS_JOB_NUMBER}`
-  },
+  },*/
   // {
   //   browserName: "MicrosoftEdge",
   //   platform: "Windows 10",
