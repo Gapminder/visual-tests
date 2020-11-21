a6d4e00d1dea889fbf4c9469f7f1188112ec88a5
abid71 <abidrana7@gmail.com>
2020-11-21 05:49:48 +0500

    added only one test execution facility for debugging

--- e2e/tests/smoke.e2e-spec.js
+++ e2e/tests/smoke.e2e-spec.js
@@ -8,56 +8,60 @@ const SHEET_KEYS = Object.keys(ALL_SHEETS);
 
 let innerWidth;
 let innerHeight;
+let singleChart;
 
 function getSheetKeys() {
-  for (let i = 0; i < SHEET_KEYS.length; i++) {
-    getEnvForSheets(SHEET_KEYS[i]);
+  for (const sheetKey of SHEET_KEYS) {
+    getEnvForSheets(sheetKey);
   }
 }
 
 function getEnvForSheets(SHEET_KEY) {
-
-  var chartKeys = Object.keys(ALL_SHEETS[`${SHEET_KEY}`]);
   var baseURL = [];
 
-  for (let i = 0; i < chartKeys.length; i++) {
+  for (const chartKey of Object.keys(ALL_SHEETS[`${SHEET_KEY}`])) {
+    if (chartKey.match(/BASE URL/gi)) {
 
-    if (chartKeys[i].match(/BASE URL/gi)) {
-
-      const envSelcted = Object.values(ALL_SHEETS[`${SHEET_KEY}`][`${chartKeys[i]}`]);
-      for (let j = 0; j < envSelcted.length; j++) {
-        baseURL.push(envSelcted[j]);
+      for (const env of Object.values(ALL_SHEETS[`${SHEET_KEY}`][`${chartKey}`])) {
+        baseURL.push(env);
       }
       break;
     }
   }
 
-  for (i = 0; i < baseURL.length; i++) {
-
-    var ENV = baseURL[i]['testName'];
-    var URL = baseURL[i]['url'];
-
-    getSuiteData(ENV, SHEET_KEY, URL);
+  for (const item of baseURL) {
+    getSuiteData(SHEET_KEY, item.testName, item.url);
   }
 }
 
-function getSuiteData(ENV, SHEET_KEY, URL) {
-
+function getSuiteData(SHEET_KEY, ENV, URL) {
   var chartKeys = Object.keys(ALL_SHEETS[`${SHEET_KEY}`]);
-  for (let j = 0; j < chartKeys.length; j++) {
 
-    if (chartKeys[j].match(/BASE URL/gi)) {
-      continue;
+  for (const chartKey of chartKeys) {
+    if (chartKey.match(/BASE URL/gi)) continue;
+
+    var chartSelcted = Object.values(ALL_SHEETS[`${SHEET_KEY}`][`${chartKey}`]);
+    for (const item of chartSelcted) {
+      if (item.testName.match(/exclusive_test/gi)) {
+        singleChart = {};
+        singleChart[chartKey] = chartSelcted.filter(function (el) { return el.testName == item.testName; });
+        break;
+      }
     }
-    suiteRunner(ENV, SHEET_KEY, URL, chartKeys[j]);
+  }
+
+  if (singleChart != null) chartKeys = Object.keys(singleChart);
+  for (const chartKey of chartKeys) {
+    if (chartKey.match(/BASE URL/gi)) continue;
+    suiteRunner(ENV, SHEET_KEY, URL, chartKey);
   }
 }
 
 function suiteRunner(ENV, SHEET_KEY, URL, CHART_KEY) {
-
   var chartSelcted = Object.values(ALL_SHEETS[`${SHEET_KEY}`][`${CHART_KEY}`]);
-  describe(`${ENV} > ${SHEET_KEY} > ${CHART_KEY}`, () => {
+  if (singleChart != null) chartSelcted = Object.values(singleChart[CHART_KEY]);
 
+  describe(`${ENV} > ${SHEET_KEY} > ${CHART_KEY}`, () => {
     for (let j = 0; j < chartSelcted.length; j++) {
       testRunner(ENV, SHEET_KEY, URL, CHART_KEY, chartSelcted[j], j + 1);
     }
@@ -92,7 +96,6 @@ function testRunner(ENV, SHEET_KEY, URL, CHART_KEY, CHART_SELECTED, INDEX) {
 
     var snapshot = `${suiteName} > ${INDEX}`;
     //snapshot = browser.name != undefined ? `${browser.name} > ${snapshot}` : snapshot;
-
     await percySnapshot(snapshot, {
       "widths": [innerWidth],
       "minHeight": innerHeight
